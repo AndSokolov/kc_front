f73edda22cb7293271fa2b08db3ddfcf8620b986
Andrey Sokolov <andr.al.sokolov@yadro.com>
2023-07-06 19:43:12 +0300

    added mf

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'files',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'manifest',
+        remoteName: 'kc_files_front',
+        exposedModule: 'kc_files_front'
+      })
+        .then(m => m.FilesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

6459f2d7c43f55840e7c7e6dd43d674811f080e6
Andrey Sokolov <andr.al.sokolov@yadro.com>
2023-12-25 14:02:28 +0300

    asa

---
+++ src/app/AuthInterceptor.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {Router} from '@angular/router';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor{
+  constructor(private router: Router) {
+  }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+      req = req.clone({ setHeaders: { Authorization: `Bearer lalalaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa` }});
+
+    return next.handle(req).pipe(
+      catchError((err: HttpErrorResponse) => {
+          if (err.status === 401){
+            this.router.navigate(['/admin', 'login']);
+          }
+          return throwError(err);
+        }
+      ));
+
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthInterceptor } from "./AuthInterceptor";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -10,9 +12,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    multi: true,
+    useClass: AuthInterceptor
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
